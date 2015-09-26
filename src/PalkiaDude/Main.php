c9ab39d5974618e7b85889757f9f79d7961858a4
PalkiaDude <teammegamanz@gmail.com>
2015-09-27 00:01:49 +0800

    Create Main.php

---
+++ src/PalkiaDude/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace MorphPlayers;
+use pocketmine/plugin/PluginBase;
+use pocketmine/event/Listener;
+use pocketmine/event/player/PlayerMoveEvent;
+use pocketmine/entity/Creeper;
+use pocketmine/Player;
+use pocketmine/level/Position;
+use pocketmine/Server;
+use pocketmine/plugin/Plugin
+class Main extends PluginBase implements Listener{
