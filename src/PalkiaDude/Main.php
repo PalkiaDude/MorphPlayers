e51b9f1a093392ce14132ff8ef52cac756ffeec1
PalkiaDude <teammegamanz@gmail.com>
2015-09-30 22:00:25 +0800

    Update Main.php

--- src/PalkiaDude/Main.php
+++ src/PalkiaDude/Main.php
@@ -20,7 +20,36 @@ class Main extends PluginBase implements Listener{
   }
 }
 
-      public function onMove(PlayerMoveEvent $newb){
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
+        if(strtolower($cmd->getName()) === "footblock"){
+            if($sender instanceof Player){
+                if($this->isPlayer($sender)){
+                    $this->removePlayer($sender);
+                    $sender->sendMessage(TextFormat::GOLD . "You have disabled FootBlock.");
+                    return true;
+                }
+                else{
+                    $this->addPlayer($sender);
+                    $sender->sendMessage(TextFormat::GREEN . "You have enabled FootBlock.");
+                    return true;
+                }
+            }
+            else{
+                $sender->sendMessage(TextFormat::RED . "Please use this command in-game.");
+                return true;
+            }
+        }
+    }
+    public function addPlayer(Player $player){
+        $this->players[$player->getName()] = $player->getName();
+    }
+    public function isPlayer(Player $player){
+        return in_array($player->getName(), $this->players);
+    }
+    public function removePlayer(Player $player){
+        unset($this->players[$player->getName()]);
+    }
+}      public function onMove(PlayerMoveEvent $newb){
         $p= $newb getPlayer();
         if(isset($this->creeper) && $p === $this->player){
           $this->creeper->teleport(new Position($p->getX(),$p->getY(),$p->getZ(),$p->getLevel()));
